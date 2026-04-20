793fa9cb5471d5ae08f704eceabaf51feddff007
paloxaal <pal.oxaal@gmail.com>
2026-04-20 23:53:30 +0200

    Add files via upload

--- builtly/__init__.py
+++ builtly/__init__.py
@@ -1,24 +1,3 @@
-"""Builtly v8 delivery 5 package."""
+"""Builtly v8 geometry engine package."""
 
-from .masterplan_types import (
-    BarnehageConfig,
-    Bygg,
-    ConceptFamily,
-    Delfelt,
-    Masterplan,
-    PlanRegler,
-    Typology,
-)
-from .masterplan_integration import OptionResult, run_concept_options
-
-__all__ = [
-    "BarnehageConfig",
-    "Bygg",
-    "ConceptFamily",
-    "Delfelt",
-    "Masterplan",
-    "OptionResult",
-    "PlanRegler",
-    "Typology",
-    "run_concept_options",
-]
+from .masterplan_types import *  # noqa: F401,F403
