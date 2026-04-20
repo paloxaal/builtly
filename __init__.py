4d1ee10b9857cf2779718df0ba7c06481c1f3ac2
paloxaal <pal.oxaal@gmail.com>
2026-04-20 11:05:02 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,33 @@
+"""Builtly v8 core package."""
+
+from .masterplan_types import (
+    BarnehageConfig,
+    Bygg,
+    ConceptFamily,
+    CourtyardKind,
+    Delfelt,
+    Masterplan,
+    PlanRegler,
+    SolBuildingResult,
+    SolReport,
+    MUAReport,
+    ComplianceCheck,
+    ComplianceState,
+    Typology,
+)
+
+__all__ = [
+    "BarnehageConfig",
+    "Bygg",
+    "ConceptFamily",
+    "CourtyardKind",
+    "Delfelt",
+    "Masterplan",
+    "PlanRegler",
+    "SolBuildingResult",
+    "SolReport",
+    "MUAReport",
+    "ComplianceCheck",
+    "ComplianceState",
+    "Typology",
+]
